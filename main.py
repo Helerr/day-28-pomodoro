72f85edcc9d8263cc2047ac171ca913aa841886b
alexandru.neculai <alleexx250595@gmail.com>
2022-03-29 15:34:55 +0300

    *timer working for Work/breaks *sessions counter working

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -9,12 +10,52 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+REPS = 0
+reps_checkmarks = ""
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global REPS, reps_checkmarks
+    REPS += 1
+    # work_sec = WORK_MIN * 60
+    # short_break_sec = SHORT_BREAK_MIN * 60
+    # long_break_sec = LONG_BREAK_MIN * 60
+    work_sec = 10
+    short_break_sec = 5
+    long_break_sec = 7
+    if REPS % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Long Break", fg=RED)
+    elif REPS % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Short Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
+        reps_checkmarks += "✔"
+        checkmarks_label.config(text=reps_checkmarks)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    if count_min < 10:
+        count_min = f"0{count_min}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        print(count)
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -26,13 +67,13 @@ window.eval('tk::PlaceWindow . center')
 tomato_image = PhotoImage(file="tomato.png")
 canvas = Canvas(width=200, height=224, background=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
 timer_label = Label(fg=GREEN, text="Timer", font=(FONT_NAME, 50, "bold"), background=YELLOW)
 timer_label.grid(row=0, column=1)
 
-start_button = Button(text="Start", highlightbackground=YELLOW)
+start_button = Button(text="Start", highlightbackground=YELLOW, command=start_timer)
 start_button.grid(row=2, column=0)
 
 checkmarks_label = Label(fg=GREEN, text="✔", font=(FONT_NAME, 24, "bold"), background=YELLOW)
